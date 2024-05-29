50803a606f8dd2de076adc7bb73038f78d56c796
dopaemon <polarisdp@gmail.com>
2024-05-29 06:28:49 +0000

    sm8450-common: Video Play has issue
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,6 +114,7 @@ BOARD_VENDOR_RAMDISK_FRAGMENTS := dlkm
 BOARD_VENDOR_RAMDISK_FRAGMENT.dlkm.KERNEL_MODULE_DIRS := top
 
 BOARD_KERNEL_CMDLINE := \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
     ktpi=off \
     irqaffinity=0-3 \
@@ -188,7 +189,6 @@ TARGET_BOARD_PLATFORM := taro
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
-TARGET_USES_VULKAN = true
 
 # Power
 TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(COMMON_PATH):libperfmgr-ext-xiaomi
