9f959a66068c0593781e87fafcb45b6a986c00cb
dopaemon <polarisdp@gmail.com>
2025-10-03 15:22:19 +0000

    sm8450-common: using device_framework_matrix from vendor
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -224,7 +224,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/vintf/device_framework_compatibility_matrix.xml \
     hardware/dolby/configs/vintf/dolby_framework_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    $(COMMON_PATH)/vintf/device_framework_matrix.xml
+    vendor/cherish/config/device_framework_matrix.xml
 
 
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/vintf/framework_manifest.xml
